ee410d14d4e7112127128f69a267165c18c39db7
xhankx <160678145+xhankx@users.noreply.github.com>
2024-11-30 21:50:34 -0600

    installed rollup and updated package.json

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import babel from '@rollup/plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from '@rollup/plugin-terser'; // Updated import
+
+const packageJson = require('./package.json');
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+    }),
+    postcss(),
+    terser(), // Using the updated terser plugin
+  ],
+};

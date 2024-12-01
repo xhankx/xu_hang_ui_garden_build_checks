9506f4b870f2a128b1a8ef946829941a52ed5a31
xhankx <160678145+xhankx@users.noreply.github.com>
2024-11-30 22:02:44 -0600

    updated tsconfig.json

--- rollup.config.js
+++ rollup.config.js
@@ -6,33 +6,35 @@ import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from '@rollup/plugin-terser';
 
+
+
 const packageJson = require('./package.json');
 
 export default {
-  input: 'src/index.ts', // Use the entry point file you've just created
-  output: [
-    {
-      file: packageJson.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: packageJson.module,
-      format: 'esm',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    peerDepsExternal(), // Prevent bundling peerDependencies
-    resolve(), // Find and bundle `node_modules`
-    commonjs(), // Convert CommonJS to ES6
-    typescript({ tsconfig: './tsconfig.json' }), // Compile TypeScript files
-    babel({
-      exclude: 'node_modules/**',
-      babelHelpers: 'bundled',
-    }),
-    postcss(), // To handle CSS imports in the components
-    terser(), // Minify the output
-  ],
-  external: Object.keys(packageJson.peerDependencies || {}),
-};
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+      }),
+      postcss(),
+      terser(), // Updated to use terser directly
+    ],
+    external: Object.keys(packageJson.peerDependencies || {}),
+  };

fb0ffabe9ebf722051ea7542d3ad2a73c6804b85
xhankx <160678145+xhankx@users.noreply.github.com>
2024-11-30 21:55:02 -0600

    fixed bugs

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,12 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
-import { terser } from '@rollup/plugin-terser'; // Updated import
+import { terser } from '@rollup/plugin-terser';
 
 const packageJson = require('./package.json');
 
 export default {
-  input: 'src/index.ts',
+  input: 'src/index.ts', // Use the entry point file you've just created
   output: [
     {
       file: packageJson.main,
@@ -23,15 +23,16 @@ export default {
     },
   ],
   plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ tsconfig: './tsconfig.json' }),
+    peerDepsExternal(), // Prevent bundling peerDependencies
+    resolve(), // Find and bundle `node_modules`
+    commonjs(), // Convert CommonJS to ES6
+    typescript({ tsconfig: './tsconfig.json' }), // Compile TypeScript files
     babel({
       exclude: 'node_modules/**',
       babelHelpers: 'bundled',
     }),
-    postcss(),
-    terser(), // Using the updated terser plugin
+    postcss(), // To handle CSS imports in the components
+    terser(), // Minify the output
   ],
+  external: Object.keys(packageJson.peerDependencies || {}),
 };
